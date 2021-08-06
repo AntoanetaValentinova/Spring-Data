3a7a91626a467d8d6890f0190932dce1573dd1c1
Antoaneta Ivanova <antoaneta.valentinova@gmail.com>
2021-08-06 17:04:55 +0300

    Add files

---
+++ 03. Hibernate Code First/Hibernate Code First-exercise 1/src/main/java/entities/BaseEntity.java
@@ -0,0 +1,22 @@
+package entities;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    private Long id;
+
+    public BaseEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
